3d740ae2bf21d7f053b313c50446a956c6266c30
t0ngk <39180519+t0ngk@users.noreply.github.com>
2023-08-31 15:02:36 +0700

    :tada:  : Initial Commit.

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-orange-500 text-white font-bold p-4">
+        <Text className="text-2xl font-bold text-center text-white">Super React Native Alpha Beta Bravo Jub Jub By T0NG</Text>
+    </View>
+  );
+}
